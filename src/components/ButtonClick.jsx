401cb98e951ef919a6e4cbe5e9981e7ac836aa0e
Beck Liu <b323liu@uwaterloo.ca>
2022-02-07 17:58:59 -0400

    added input sanitation, made small changes to fetchTheme function

--- src/components/ButtonClick.jsx
+++ src/components/ButtonClick.jsx
@@ -2,118 +2,77 @@ import React,{useState, useEffect} from "react";
 import {Input} from 'antd';
 
 
-
-function FetchData(theurl) {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
-
-
-  useEffect(() => {
-    fetch(theurl)
-    //https://api.github.com/users/mralexgray/repos
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setItems(result);
-        },
-
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
-  }, [])
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <div className="Result">
-        {items.map(item => (
-        <p key={item.id}>
-          {item.name}
-        </p>
-        ))}
-      </div>
-  );
-  }
-}
-
-
-
-
 function ButtonClick(){
   const [url,setUrl] = useState(null);
   const [print,setPrint] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
   const [theme, setTheme] = useState(" asd");
+  const [click,setClick] = useState(false);
+
 
   function getUrl(val){
+
     setUrl(val.target.value)
+    setClick(false);
+    setIsLoaded(false);
+
     setPrint(false)
+
   }
 
-  async function fetchTheme() {
 
-    await fetch(`https://us-central1-get-theme.cloudfunctions.net/getTheme?myshopify=${url}`)
-    .then(response => response.text())
-    .then(data => setTheme(data));
+  async function fetchTheme() {
+    setClick(true);
+
+    if (url.includes('https://') == false){
+      await fetch(`https://us-central1-get-theme.cloudfunctions.net/getTheme?myshopify=https://${url}`)
+      .then(response => response.text())
+      .then((data) => {
+        setIsLoaded(true);
+        setTheme(data);
+      });
+
+    }else{
+      await fetch(`https://us-central1-get-theme.cloudfunctions.net/getTheme?myshopify=${url}`)
+      .then(response => response.text())
+      .then((data) => {
+        setIsLoaded(true);
+        setTheme(data);
+      });
+    }
 
     console.log(theme);
     setPrint(true);
   }
 
-  return (
-    <div>
-    {
-      print?
+    return (
       <div>
-{/* This part (FetchData) is not working */}
-    {/* {FetchData(url)} */}
+      {
+        !isLoaded && click?
+        <div>
+        <p className="Result">Loading...</p>
+        </div>
+        :
+        print?
+        <div>
+        <p className="Result">{"The theme detected is: "+ theme}</p>
+        </div>
+        :" "
+      }
+      <Input className = "input" placeholder="Enter URL: " onChange={getUrl}/> 
+  
+      <button class="button" onClick={fetchTheme}>Detect</button>
 
-      <p className="Result">{"The url entered is: "+ theme}</p>
       </div>
-      :null
-    }
-    <Input className = "input" placeholder="Enter URL: " onChange={getUrl}/> 
+    )
 
-    <button class="button" onClick={fetchTheme}>Detect</button>
+  
 
-    </div>
-  )
 
 }
 
 
 
-// class ButtonClick extends React.Component {
-//     constructor(props) {
-//       super(props);
-//       this.state = {
 
-//         url:""
-//       };
-//     }
-//     detectUrl = () => {
-//       this.setState({url:"No url was detected."});
-//     }
-//     render() {
-//       return (
-//         <div>
-//           <p className="DetectedUrl">
-//             {this.state.url}
-//           </p>
-//           <button
-//             class="button"
-//             onClick={this.detectUrl}
-//           >Detect</button>
-//         </div>
-
-//       );
-//     }
-//   }
 
 export default ButtonClick;
